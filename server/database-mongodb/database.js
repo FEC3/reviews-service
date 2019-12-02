7dea4270fec4b262114ffbd006bb6451e59476a9
[shuwei] <zhang_s1@gwu.edu>
2019-12-02 13:27:41 -0800

    Finished schema

--- server/database-mongodb/database.js
+++ server/database-mongodb/database.js
@@ -6,9 +6,25 @@ mongoose.connect(mongoUri, {useNewUrlParser: true, useUnifiedTopology: true}); /
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error：'));
 
-let reviewSchema = mongoose.Schema({ })
+let reviewSchema = mongoose.Schema({
+  product_id: Number,
+  date: { type: Date, default: Date.now },
+  author: String,
+  title: String,
+  body: String,
+  recommend: Boolean,
+
+  overall_rating : { type: Number, min: 1, max: 5 },
+  value_rating: {type: Number, min: 1, max: 5},
+  quality_rating: {type: Number, min: 1, max: 5},
+  appearance_rating: {type: Number, min: 1, max: 5},
+  works_as_expected_rating: {type: Number, min: 1, max: 5},
+
+  helpful_count: Number,
+  not_helful_count: Number
+});
 
 let Review = mongoose.model('Review', reviewSchema);
 
 
-module.exports = db;
+module.exports = reviewSchema;
