0714688ce11017a7f3e44deacf7332ab07c8444f
[shuwei] <zhang_s1@gwu.edu>
2019-12-02 00:05:58 -0800

    Finished Recommendation part, need to add stars, bars, and change layout

--- client/src/components/ReviewListEntry.jsx
+++ client/src/components/ReviewListEntry.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
-const ReviewListEntry = (props) => {
-  return (
-    <div>
-      <h1>{props.review.title}</h1>
-      {props.review.body}
-      {props.review.recommend}
-      {props.review.helpful_count}
-    </div>
-  )
+var moment = require('moment');
+class ReviewListEntry extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  ifrecommend(recommend){
+    if(recommend === 1){
+      return "Yes, I recommend this product.";
+    }else{
+      return "No, I do not recommend this product.";
+    }
+  }
+  render(){
+    return (
+      <div>
+        <div>
+        <span>{this.props.review.author}</span>
+
+        <span>{moment(this.props.review.createdAt).fromNow()}</span>
+        </div>
+        <div>
+          {this.props.review.title}
+          <p>{this.props.review.body}</p>
+          <div>{this.ifrecommend(this.props.review.recommend)}</div>
+          Helpful? {this.props.review.helpful_count}
+        </div>
+      </div>
+    )
+    }
+
 }
 
 export default ReviewListEntry;
--- client/src/sample_data.js
+++ client/src/sample_data.js
@@ -5,7 +5,7 @@ const samplePosts = [
     title: 'Calling Ikea...did I get a bad one?',
     createdAt:'2017-11-14T05:57:26.037Z',
     body: `I bought this six months ago and already, it looks terrible. Admittedly, when it came out of the box it had TONS of dents and indentations in the upholstery. Recently saw a friend's version of the same couch, and it looked MUCH nicer. Don't know...maybe I got a bad one?`,
-    recommend: 'No, I do not recommend this product',
+    recommend: 0,
     helpful_count: '3',
     not_helpful_count: '0'
   },
@@ -15,7 +15,7 @@ const samplePosts = [
     title: 'Love it',
     createdAt:'2017-11-14T05:57:26.037Z',
     body: `I spend so long looking for the perfect sofa: fabric that was dark & tightly weaved enough to be toddler/cat friendly, big enough for all of us to watch tv together but not take up the whole room, classic design elements that fit with modern decor, soft but still has those clean lines. This is perfect. I got dark gray w/ light wood legs, a few throw pillows of differing sizes and textures, and a big faux fur sheepskin to drape over the back so my cats can lounge without getting their fur everywhere.`,
-    recommend: 'Yes, I recommend this product.',
+    recommend: 1,
     helpful_count: '18',
     not_helpful_count: '1'
   },
@@ -25,7 +25,7 @@ const samplePosts = [
     title: 'Love it',
     createdAt:'2017-11-14T05:57:26.037Z',
     body: `Beautiful sofa, excellent quality love this purchase`,
-    recommend: 'Yes, I recommend this product.',
+    recommend: 1,
     helpful_count: '4',
     not_helpful_count: '0'
   }
