3068d2dca09dd2582cb8f14f2fe8a6def9715ad8
[shuwei] <zhang_s1@gwu.edu>
2019-12-10 14:52:33 -0800

    Adding access headers in server

--- server/index.js
+++ server/index.js
@@ -11,10 +11,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 const PORT = process.env.PORT || 3003;
 app.get("/reviews/:id", (req,res)=> {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   let id = parseInt(req.params.id);
   console.log("====id:",id)
   Reviews.fetchData(id).then(data=>res.send(data))
-
 });
 
 app.listen(PORT, () => {
